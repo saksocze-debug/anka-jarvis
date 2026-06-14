fde5706d9f808341fc9619c74c68a55b45f3f112
saksocze-debug <saksocze@gmail.com>
2026-06-15 02:11:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const allowedOrigin = process.env.FRONTEND_ORIGIN || '*';
 
 app.use(cors({ origin: allowedOrigin }));
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 const limiter = rateLimit({
   windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS || '60000', 10),
